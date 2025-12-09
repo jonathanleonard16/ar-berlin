ccaf54d7fccb087b48212385138f27939835c304
jonathan <jonathanleonard86@gmail.com>
2025-12-09 16:42:30 +0100

    AppContainer: added ARScanManager

--- app/src/main/java/com/jonathan/arberlin/di/AppContainer.kt
+++ app/src/main/java/com/jonathan/arberlin/di/AppContainer.kt
@@ -6,10 +6,12 @@ import com.jonathan.arberlin.data.repository.LocationRepository
 import com.jonathan.arberlin.data.repository.LocationRepositoryImpl
 import com.jonathan.arberlin.data.repository.PoiRepository
 import com.jonathan.arberlin.data.repository.PoiRepositoryImpl
+import com.jonathan.arberlin.domain.ARScanManager
 
 interface AppContainer {
     val poiRepository: PoiRepository
     val locationRepository: LocationRepository
+    val arScanManager: ARScanManager
 }
 
 class DefaultAppContainer(private val context: Context): AppContainer {
@@ -24,4 +26,9 @@ class DefaultAppContainer(private val context: Context): AppContainer {
     override val locationRepository: LocationRepository by lazy {
         LocationRepositoryImpl(context)
     }
+
+    override val arScanManager: ARScanManager by lazy {
+        ARScanManager()
+    }
+
 }
